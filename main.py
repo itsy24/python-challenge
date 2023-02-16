034c84dcc96a8e6e7fbebd4af806fb699b8242ce
itsy24 <isy@alaska.edu>
2023-02-15 22:02:56 -0900

    csv data sets commits not final

---
+++ main.py
@@ -0,0 +1,18 @@
+#pybank
+
+#import modules
+import os, csv
+
+csvpath = os.path.join("..","Resources","budget_data.csv")
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimitier = "-")
+    print(csvreader)
+
+# total number of months in the dataset
+# net total amount of profit/losses
+# net average of profit/losses
+# greatest inc in profit for the entire period
+# greatest dec in profit for the entire period
+#print to terminal
+#export text file
