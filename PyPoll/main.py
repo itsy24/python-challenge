e924e4b9efdaf74854a83d9f7bd2ef459985b808
itsy24 <isy@alaska.edu>
2023-02-18 23:45:39 -0900

    completed assignment

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,6 @@
 
 #import modules
 import os, csv
-from itertools import cycle
 
 csvpath = os.path.join("C:\\Users\\itsy7\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\Resources\\election_data.csv")
 
@@ -12,6 +11,7 @@ candidate_list = []
 votercount1= 0
 votercount2 = 0
 votercount3 = 0
+winner = 0
 
 
 with open(csvpath) as csvfile:
@@ -35,8 +35,8 @@ with open(csvpath) as csvfile:
             votercount1 += 1
         if i[2] == voter2:
             votercount2 += 1
-        if i[2] == voter2:
-            votercount3 += 1           
+        if i[2] == voter3:
+            votercount3 += 1       
 
 # % votes/candidate
     percent1 = (votercount1/total_votes) * 100
@@ -46,15 +46,16 @@ with open(csvpath) as csvfile:
     percent2 = round(percent2,3)
 
     percent3 = (votercount3/total_votes) * 100
-    percent3 = round(percent2,3)
+    percent3 = round(percent3,3)
     
-
-
-    
-
-
 
 # Winner based on popular vote
+    if votercount1 > votercount2 and votercount1 > votercount3:
+        winner = voter1 
+    elif votercount2 > votercount1 and votercount2 > votercount3:
+        winner = voter2
+    elif votercount3 > votercount1 and votercount3 > votercount2:
+        winner = voter3
 
 #print to terminal
 print(f""" 
@@ -62,6 +63,22 @@ Election Results
 -----------------------
 Total Votes: {total_votes}   
 -----------------------
-
-                               """)
+{candidate_list[0]}: {percent1}% ({votercount1})
+{candidate_list[1]}: {percent2}% ({votercount2})
+{candidate_list[2]}: {percent3}% ({votercount3})
+-----------------------
+Winner: {winner}
+-----------------------""")
 #export text file
+with open("C:\\Users\\itsy7\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\Analysis\\Analysis.csv","w", newline="") as csvfile:
+    csvfile.write(f""" 
+Election Results
+-----------------------
+Total Votes: {total_votes}   
+-----------------------
+{candidate_list[0]}: {percent1}% ({votercount1})
+{candidate_list[1]}: {percent2}% ({votercount2})
+{candidate_list[2]}: {percent3}% ({votercount3})
+-----------------------
+Winner: {winner}
+-----------------------""")
