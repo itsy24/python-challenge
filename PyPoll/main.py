8f5799bb7c039896b109e51f8340209606eedff5
itsy24 <isy@alaska.edu>
2023-02-18 23:21:23 -0900

    updated pypoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,14 +2,16 @@
 
 #import modules
 import os, csv
+from itertools import cycle
 
 csvpath = os.path.join("C:\\Users\\itsy7\\OneDrive\\Documents\\GitHub\\python-challenge\\PyPoll\\Resources\\election_data.csv")
 
 #Create Variables
 total_votes = 0
 candidate_list = []
-votes= 0
-votes2 = 0
+votercount1= 0
+votercount2 = 0
+votercount3 = 0
 
 
 with open(csvpath) as csvfile:
@@ -17,23 +19,49 @@ with open(csvpath) as csvfile:
     csvheader = next(csvreader) 
     election_data = list(csvreader)
 
+#total votes casted / candidate names
     for i in election_data:
         total_votes += 1
+        candidate_name = i[2]
         if i[2] not in candidate_list:
             candidate_list += [i[2]]
-        if i[2] == candidate_list[0]:
-            votes += 1
-        if i[2] == candidate_list[0]:
-            votes += 1
-      
-print(candidate_list)
-print(votes)
+
+# total votes/candidate
+    for i in election_data:
+        voter1 = candidate_list[0]
+        voter2 = candidate_list[1]
+        voter3 = candidate_list[2]
+        if i[2] == voter1:
+            votercount1 += 1
+        if i[2] == voter2:
+            votercount2 += 1
+        if i[2] == voter2:
+            votercount3 += 1           
+
+# % votes/candidate
+    percent1 = (votercount1/total_votes) * 100
+    percent1 = round(percent1,3)
+
+    percent2 = (votercount2/total_votes) * 100
+    percent2 = round(percent2,3)
+
+    percent3 = (votercount3/total_votes) * 100
+    percent3 = round(percent2,3)
     
 
-# total number of months in the dataset
-# net total amount of profit/losses
-# net average of profit/losses
-# greatest inc in profit for the entire period
-# greatest dec in profit for the entire period
+
+    
+
+
+
+# Winner based on popular vote
+
 #print to terminal
+print(f""" 
+Election Results
+-----------------------
+Total Votes: {total_votes}   
+-----------------------
+
+                               """)
 #export text file
